27f6778502b3acb4c48583731ce9f2b6399b9068
dbto1970 <donbartojunior@gmail.com>
2021-09-09 16:26:05 -0400

    fixed export

--- Components/GoodComponent.js
+++ Components/GoodComponent.js
@@ -4,6 +4,8 @@ import BusinessCard from './BusinessCard';
 const GoodBusiness = () => {
 
     return (
-        <BusinessCard rateClass='Good' item={array[0]}
+        <BusinessCard rateClass='Good' item={array[0]} />
     );
 }
+
+export default GoodBusiness;
